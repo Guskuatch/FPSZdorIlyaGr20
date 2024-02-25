da628bda9379bb1385a0ec0c33ad77773dabd8b3
Guskquatch <alesoloxey@gmail.com>
2024-02-25 22:10:10 +0300

    250224TPS

---
+++ Assets/Scripts/FireBall.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+    public float damage = 10f;
+
+    void Start()
+    {
+        Invoke("DestoryFireBall", lifetime);
+    }
+
+    private void FixedUpdate()
+    {
+        MoveFixedUpdate();
+    }
+
+    private void MoveFixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.fixedDeltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        DamageEnemy(collision);
+        DestoryFireBall();
+    }
+
+    private void DamageEnemy(Collision collision)
+    {
+        var enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+        if (enemyHealth != null)
+        {
+            enemyHealth.DealDamage(damage);
+        }
+    }
+
+    private void DestoryFireBall()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/FireBallCaster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallCaster : MonoBehaviour
+{
+    public FireBall fireballPrefab;
+    public Transform fireBallSourceTransform;
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        FireballCaste();
+    }
+
+    private void FireballCaste()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireballPrefab, fireBallSourceTransform.position, fireBallSourceTransform.rotation);
+        }
+    }
+}
